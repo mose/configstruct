63705dfa680c2f11a0db3f1b5b2cec0432498d5f
mose <mose@mose.com>
2015-08-04 02:54:13 +0800

    add some code documentation

--- lib/configstruct.rb
+++ lib/configstruct.rb
@@ -1,8 +1,14 @@
 require 'ostruct'
 require 'yaml'
 
+# the Configstruct object
+# inherits from OpenStruct and adds some features:
+# - set default values
+# - redirects output and input IO to custom streams
+# - writes file on disk as yaml
 class ConfigStruct < OpenStruct
 
+  # creates new object
   def initialize(options = nil, input = STDIN, output = STDOUT, unless_set = false)
     super(options)
     @input = input
@@ -13,15 +19,18 @@ class ConfigStruct < OpenStruct
     addvalues
   end
 
+  # sets defaults value for initializer
   def set_defaults
     default :basedir, '/tmp'
     default :basefile, File.join(self.basedir, 'config.yml')
   end
 
+  # creates dir where to store config if it does not exist
   def prepare_dirs
     FileUtils.mkdir_p self.basedir unless Dir.exist? self.basedir
   end
 
+  # adds value in the configuration hash
   def addvalues
     setup unless File.exist? self.basefile
     YAML.load_file(self.basefile).each do |k, v|
@@ -32,32 +41,39 @@ class ConfigStruct < OpenStruct
     end
   end
 
+  # creates a blank config file with empty values
   def setup
     write Hash.new
   end
 
+  # attributes value if it is nil
   def default(var, value)
     send(var).nil? && send("#{var}=", value)
   end
 
+  # writes config faile on disk as yaml
   def write(values)
     File.open(self.basefile, 'w') do |f|
       f.write YAML.dump(values)
     end
   end
 
+  # redirect puts to the initialized output
   def puts(*string)
     @output.puts *string
   end
 
+  # redirect print to the initialized output
   def print(*string)
     @output.print *string
   end
 
+  # redirect printf to the initialized output
   def printf(string, *args)
     @output.printf string, *args
   end
 
+  # redirect gets fromm the initialized input
   def gets(*args)
     @input.gets *args
   end
